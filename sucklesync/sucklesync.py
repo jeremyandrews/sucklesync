a345188c0ea4bbf178a7db9a9c85deb5094c6ae1
Jeremy Andrews <jeremy@tag1consulting.com>
2017-06-29 22:23:41 +0200

    load logging and email preferences from configuration file

--- sucklesync/sucklesync.py
+++ sucklesync/sucklesync.py
@@ -9,7 +9,7 @@ from config import config
 
 __version__ = "0.1"
 
-DEFAULT_CONFIG    = ['/etc/sucklesync.cfg', '/usr/local/etc/sucklesync.cfg', '~/.sucklesync.cfg', './sucklesync.cfg']
+DEFAULT_CONFIG    = ["/etc/sucklesync.cfg", "/usr/local/etc/sucklesync.cfg", "~/.sucklesync.cfg", "./sucklesync.cfg"]
 DEFAULT_USER      = "daemon"
 DEFAULT_GROUP     = "daemon"
 DEFAULT_LOGLEVEL  = logging.WARNING
@@ -23,6 +23,8 @@ class SuckleSync:
         self.binaries = {}
         self.flags = {}
         self.paths = []
+        self.logging = {}
+        self.mail = {}
 
     def _load_debugger(self):
         import logging.handlers
@@ -35,17 +37,36 @@ class SuckleSync:
         self.configuration = config.Config(self.debugger)
 
         # load binary paths
-        self.binaries['local_rsync'] = self.configuration.GetText("Binaries", "local_rsync", "/usr/bin/rsync")
-        self.binaries['remote_find'] = self.configuration.GetText("Binaries", "remote_find", "/usr/bin/find")
+        self.binaries["local_rsync"] = self.configuration.GetText("Binaries", "local_rsync", "/usr/bin/rsync")
+        self.binaries["remote_find"] = self.configuration.GetText("Binaries", "remote_find", "/usr/bin/find")
 
         # load flags used when launching binaries
-        self.flags['rsync_flags'] = self.configuration.GetText("Flags", "rsync_flags", "-aP")
-        self.flags['find_flags'] = self.configuration.GetText("Flags", "find_flags", "-mmin -5 -print")
+        self.flags["rsync_flags"] = self.configuration.GetText("Flags", "rsync_flags", "-aP")
+        self.flags["find_flags"] = self.configuration.GetText("Flags", "find_flags", "-mmin -5 -print")
 
         # load paths that will be suckle-synced
-        self.paths = self.configuration.GetItemPairs("Sucklepaths", ['source', 'destination'])
+        self.paths = self.configuration.GetItemPairs("Sucklepaths", ["source", "destination"])
 
-        print self.binaries, self.paths, self.flags
+        # load logging preferences
+        self.logging["filename"] = self.configuration.GetText("Logging", "filename", "/var/log/sucklesync/sucklesync.log", False)
+        self.logging["pidfile"] = self.configuration.GetText("Logging", "filename", "/var/run/sucklesync.pid", False)
+        self.logging["level"] = self.configuration.GetText("Logging", "level", "WARNING", False)
+
+        # load email preferences
+        self.mail["enabled"] = self.configuration.GetBoolean("Email", "enabled", False, False)
+        if self.mail["enabled"]:
+            required = True
+        else:
+            required = False
+        self.mail["to"] = self.configuration.GetEmailList("Email", "to", None, required)
+        self.mail["from"] = self.configuration.GetEmailList("Email", "from", None, required)
+        self.mail["hostname"] = self.configuration.GetInt("Email", "smtp_hostname", None, required)
+        self.mail["port"] = self.configuration.GetInt("Email", "smtp_port", 587, required)
+        self.mail["mode"] = self.configuration.GetText("Email", "smtp_mode", None, required)
+        self.mail["username"] = self.configuration.GetText("Email", "smtp_username", None, required)
+        self.mail["password"] = self.configuration.GetText("Email", "smtp_password", None, required)
+
+        print self.binaries, self.paths, self.flags, self.mail
 
 def start(ss):
     print "starting sucklesync ..."
